ec8fe19171bac14d2ea87060199a5d7382f54278
Rohit Maske <maskerohit004@gmail.com>
2025-08-13 00:23:31 +0530

    25777

--- home/views.py
+++ home/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Restaurant
+
+def home_view(request):
+    # Get or create the restaurant instance
+        restaurant, created = Restaurant.objects.get_or_create(
+                id=1,
+                        defaults={
+                                    'name': 'Tasty Bites',
+                                                'description': 'Best food in town!'
+                                                        }
+                                                            )
+                                                                return render(request, 'home.html', {'restaurant': restaurant})
+
+                                                                def about_view(request):
+                                                                    restaurant = Restaurant.objects.first()
+                                                                        return render(request, 'about.html', {'restaurant': restaurant})
+
+                                                                        def custom_404(request, exception):
+                                                                            return render(request, '404.html', status=404)
