8fd259fea1be1bcf5623a63305883619d915c911
Rohit Maske <maskerohit004@gmail.com>
2025-08-13 00:24:01 +0530

    25777

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import home_view
 
 urlpatterns = [
-    
-]
+    path('', home_view, name='home'),
+        path('about/', about_view, name='about'),
+        ]
