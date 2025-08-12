b9da9d9dcb59e07ce8daa8de658b7feff8ab949a
Rohit Maske <maskerohit004@gmail.com>
2025-08-13 00:22:43 +0530

    25777

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import Restaurant
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'opening_time', 'closing_time')
+        fields = ('name', 'description', 'logo', 'opening_time', 'closing_time')
+            fieldsets = (
+                    ('Basic Info', {
+                                'fields': ('name', 'description', 'logo')
+                                        }),
+                                                ('About Page', {
+                                                            'fields': ('about_us', 'image')
+                                                                    }),
+                                                                            ('Hours', {
+                                                                                        'fields': ('opening_time', 'closing_time')
+                                                                                                }),
+                                                                                                    )
